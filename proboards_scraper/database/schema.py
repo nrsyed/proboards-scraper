f5a239b9a6604c175fb7de56bb51e1f359d94c20
Najam Syed <najam.r.syed@gmail.com>
2021-06-16 21:44:42 -0400

    Make number the primary key

--- proboards_scraper/database/schema.py
+++ proboards_scraper/database/schema.py
@@ -1,4 +1,3 @@
-# TODO: should numbers be the primary keys?
 from sqlalchemy import (
     Boolean, Column, Integer, ForeignKey, String, Text, UniqueConstraint
 )
@@ -11,10 +10,9 @@ Base = declarative_base()
 class User(Base):
     """
     Attributes:
-        id (int): Primary key.
         number (int): User number obtained from the user's profile URL,
             eg, ``https://yoursite.proboards.com/user/21`` refers to the user
-            with user number 23. This should be unique for each user.
+            with user number 23.
         age (int): Optional
         birthdate (str): Optional
         date_registered (str): Unix timestamp
@@ -45,8 +43,7 @@ class User(Base):
     """
     __tablename__ = "user"
 
-    id = Column("id", Integer, primary_key=True)
-    number = Column("number", Integer, nullable=False, unique=True)
+    number = Column("number", Integer, primary_key=True, autoincrement=False)
 
     age = Column("age", Integer)
     birthdate = Column("birthdate", String)
@@ -68,54 +65,49 @@ class User(Base):
 
     # One-to-many mapping of a user and all posts they've made or threads
     # they've started.
-    posts = relationship("Post", foreign_keys="Post.user_id")
+    posts = relationship("Post", foreign_keys="Post.user_number")
     threads = relationship("Thread")
 
 
 class Category(Base):
     """
     Attributes:
-        id (int): Primary key.
-        name (str): Category name.
         number (int): Category number obtained from the main page source.
+        name (str): Category name.
 
         boards: This category's boards (including sub-boards).
     """
     __tablename__ = "category"
 
-    id = Column("id", Integer, primary_key=True)
+    number = Column("number", Integer, primary_key=True, autoincrement=False)
     name = Column("name", String, nullable=False)
-    number = Column("number", Integer, nullable=False, unique=True)
 
     boards = relationship("Board")
-
 
 
 class Board(Base):
     """
     Attributes:
-        id (int): Primary key.
-        name (str): Board name (required).
         number (int): Board number obtained from the board URL, eg,
             ``https://yoursite.proboards.com/board/42/general`` refers to the
-            "General" board having board number 42. This should be unique.
+            "General" board having board number 42.
+        name (str): Board name (required).
         url (str): 
 
-        category_id (int): Category to which this board belongs.
-        parent_id (int): Parent board primary key (if a sub-board).
+        category_number (int): Category to which this board belongs.
+        parent_number (int): Parent board primary key (if a sub-board).
 
         sub_boards: This board's sub-boards.
         threads: This board's threads.
     """
     __tablename__ = "board"
 
-    id = Column("id", Integer, primary_key=True)
+    number = Column("number", Integer, primary_key=True, autoincrement=False)
     name = Column("name", String, nullable=False)
-    number = Column("number", Integer, nullable=False, unique=True)
     url = Column("url", String)
 
-    category_id = Column("category_id", ForeignKey("category.id"))
-    parent_id = Column("parent_id", ForeignKey("board.id"))
+    category_number = Column("category_number", ForeignKey("category.number"))
+    parent_number = Column("parent_number", ForeignKey("board.number"))
 
     sub_boards = relationship("Board")
     threads = relationship("Thread")
@@ -124,28 +116,31 @@ class Board(Base):
 class Thread(Base):
     """
     Attributes:
-        id (int): Primary key.
-        locked (bool):
         number: Thread number from thread URL.
+        locked (bool):
         title (str): Thread title.
         url (str): Original URL.
 
-        board_id (int): Board (in ``board`` table) where the thread was made.
-        user_id (int): User (in ``user`` table) who started the thread.
+        board_number (int): Board (in ``board`` table) where the thread was made.
+        user_number (int): User (in ``user`` table) who started the thread.
 
         posts: This thread's posts.
     """
     __tablename__ = "thread"
 
-    id = Column("id", Integer, primary_key=True)
+    number = Column("number", Integer, primary_key=True, autoincrement=False)
+
     # TODO: default for locked, check bool type
     locked = Column("locked", Boolean)
-    number = Column("number", Integer, nullable=False, unique=True)
     title = Column("title", String)
     url = Column("url", String)
 
-    board_id = Column("board_id", ForeignKey("board.id"), nullable=False)
-    user_id = Column("user_id", ForeignKey("user.id"), nullable=False)
+    board_number = Column(
+        "board_number", ForeignKey("board.number"), nullable=False
+    )
+    user_number = Column(
+        "user_number", ForeignKey("user.number"), nullable=False
+    )
 
     posts = relationship("Post")
 
@@ -153,7 +148,7 @@ class Thread(Base):
 class Post(Base):
     """
     Attributes:
-        id (int): Primary key.
+        number (int):
         date (str): When the post was made (Unix timestamp).
         last_edited (str): When the post was last edited (Unix timestamp); if
             never, this field should be null.
@@ -166,16 +161,19 @@ class Post(Base):
     """
     __tablename__ = "post"
 
-    id = Column("id", Integer, primary_key=True)
+    number = Column("number", Integer, primary_key=True, autoincrement=False)
     date = Column("date", String)
     last_edited = Column("last_edited", String)
-    number = Column("number", Integer, nullable=False, unique=True)
     message = Column("message", String)
     url = Column("url", String)
 
-    edit_user_id = Column("edit_user_id", ForeignKey("user.id"))
-    thread_id = Column("thread_id", ForeignKey("thread.id"), nullable=False)
-    user_id = Column("user_id", ForeignKey("user.id"), nullable=False)
+    edit_user_number = Column("edit_user_number", ForeignKey("user.number"))
+    thread_number = Column(
+        "thread_number", ForeignKey("thread.number"), nullable=False
+    )
+    user_number = Column(
+        "user_number", ForeignKey("user.number"), nullable=False
+    )
 
 
 #class Poll(Base):
