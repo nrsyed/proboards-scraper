f573c26e18be4ee755fdd79e1d4403bb7dc678f4
Najam Syed <najam.r.syed@gmail.com>
2021-06-29 17:25:15 -0400

    Restructure scraper functionality and http get functionality

---
+++ proboards_scraper/http_requests.py
@@ -0,0 +1,148 @@
+import asyncio
+import http
+import imghdr
+import logging
+import os
+import pathlib
+import time
+from typing import List
+
+import aiohttp
+import bs4
+import selenium.webdriver
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_login_cookies(
+    home_url: str, username: str, password: str, page_load_wait: int = 1
+) -> dict:
+    """
+    TODO
+    """
+    chrome_opts = selenium.webdriver.ChromeOptions()
+    chrome_opts.headless = True
+    driver = selenium.webdriver.Chrome(options=chrome_opts)
+    driver.get(home_url)
+    time.sleep(page_load_wait)
+
+    links = driver.find_elements_by_tag_name("a")
+    login_url = None
+
+    for link in links:
+        href = link.get_attribute("href")
+        if href.startswith("https://login.proboards.com/login"):
+            login_url = href
+            break
+
+    # Navigate to login page and fill in username/password fields.
+    driver.get(login_url)
+    time.sleep(page_load_wait)
+
+    email_input = None
+    password_input = None
+    submit_input = None
+
+    inputs = driver.find_elements_by_tag_name("input")
+    for input_ in inputs:
+        try:
+            input_name = input_.get_attribute("name")
+            if input_name == "email":
+                email_input = input_
+            elif input_name == "password":
+                password_input = input_
+            elif input_name == "continue":
+                submit_input = input_
+        except:
+            # TODO
+            pass
+
+    email_input.send_keys(username)
+    password_input.send_keys(password)
+    submit_input.click()
+    time.sleep(page_load_wait)
+
+    cookies = driver.get_cookies()
+    return cookies
+
+
+def get_login_session(cookies: List[dict]) -> aiohttp.ClientSession:
+    """
+    TODO
+    """
+    logger.debug("Creating aiohttp login session from cookies")
+    session = aiohttp.ClientSession()
+
+    morsels = {}
+    for cookie in cookies:
+        # https://docs.python.org/3/library/http.cookies.html#morsel-objects
+        morsel = http.cookies.Morsel()
+        morsel.set(cookie["name"], cookie["value"], cookie["value"])
+        morsel["domain"] = cookie["domain"]
+        morsel["httponly"] = cookie["httpOnly"]
+        morsel["path"] = cookie["path"]
+        morsel["secure"] = cookie["secure"]
+
+        # NOTE: ignore expires field; if it's absent, the cookie remains
+        # valid for the duration of the session.
+        #if "expiry" in cookie:
+        #    morsel["expires"] = cookie["expiry"]
+
+        morsels[cookie["name"]] = morsel
+
+    session.cookie_jar.update_cookies(morsels)
+
+    logger.debug("Added cookies to aiohttp session")
+    return session
+
+
+async def get_source(
+    url: str, session: aiohttp.ClientSession
+) -> bs4.BeautifulSoup:
+    """
+    TODO
+    """
+    logger.debug(f"Getting page source for {url}")
+    # TODO: check response HTTP status code
+    resp = await session.get(url)
+    text = await resp.text()
+    return bs4.BeautifulSoup(text, "html.parser")
+
+
+async def download_image(
+    url: str, session: aiohttp.ClientSession, dst_dir: pathlib.Path
+):
+    """
+    Args:
+        url: Image URL.
+        session: aiohttp session.
+        dst_dir: Directory to which the image should be downloaded.
+    """
+    if url.startswith("//"):
+        url = f"https:{url}"
+
+    response = await session.get(url)
+
+    retval = None
+    if response.status == 200:
+        img = await response.read()
+
+        orig_fname = os.path.split(url)[1]
+        fstem = orig_fname.splitext(orig_fname)[0]
+
+        # The file extension doesn't necessarily match the filetype, so we
+        # manually check the file header and set the correct extension.
+        filetype = imghdr.what(None, h=img)
+        new_fname = f"{fstem}.{filetype}"
+
+        fpath = dst_dir / new_fname
+
+        # TODO: write image (with appropriate filename) to disk
+        breakpoint()
+    else:
+        # TODO: handle unsuccessful get
+        pass
+
+
+
