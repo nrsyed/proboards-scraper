800decbcb2d10280a1fa989584fabaee0640260c
Najam Syed <najam.r.syed@gmail.com>
2021-06-30 12:06:17 -0400

    Add function to get chrome driver and add a driver attribute to ScraperManager

--- proboards_scraper/scraper_manager.py
+++ proboards_scraper/scraper_manager.py
@@ -1,9 +1,14 @@
 import asyncio
+import logging
 import pathlib
 
 import aiohttp
+import selenium.webdriver
 
 from proboards_scraper.database import Database
+
+
+logger = logging.getLogger(__name__)
 
 
 class ScraperManager:
@@ -12,18 +17,27 @@ class ScraperManager:
         db: Database,
         client_session: aiohttp.ClientSession,
         content_queue: asyncio.Queue = None,
-        user_queue: asyncio.Queue = None,
-        image_dir: pathlib.Path = None
+        driver: selenium.webdriver.Chrome = None,
+        image_dir: pathlib.Path = None,
+        user_queue: asyncio.Queue = None
     ):
         """
         Args:
             db:
             client_session:
             content_queue:
+            driver:
+            image_dir:
             user_queue:
         """
         self.db = db
         self.client_session = client_session
+
+        if driver is None:
+            logger.warning(
+                "Polls cannot be scraped without setting a Chrome webdriver"
+            )
+        self.driver = driver
 
         if image_dir is None:
             image_dir = pathlib.Path("./images").expanduser().resolve()
